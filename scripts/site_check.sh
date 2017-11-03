174816ae608c6bf01779975ec1368581b5637436
Tania Allard <t.allard@sheffield.ac.uk>
2017-11-03 14:29:59 +0000

    Fixed Travis scripts

--- scripts/site_check.sh
+++ scripts/site_check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build
-bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx
+bundle exec jekyll build  # builds the site
+bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx # checks that there are no broke links 
