134494b71e3dbf1f0904d642bfdda0b6d28dd6a0
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-12 15:40:34 +0000

    Removed scripts to be replaced with nbjekyll

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# We use nbval to validate the notebooks and generate the
-# corresponding badge for jekyll
 
-echo "***** Testing notebooks *****"
-py.test --nbval-lax */*.ipynb
-
-echo "***** Converting notebooks to markdown"
-
-jupyter nbconvert --config scripts/mock.py
-
-
-echo "***** Final touches"
-python scripts/nb_replace.py
-
-clear
-echo "Notebooks ready"
-
-source deactivate
+# The conversion of the notebooks is done via the nbjekyll package
+# https://github.com/trallard/nbconvert-jekyllconvert
