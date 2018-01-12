9c06646bb368f95fc0365b6443d6cebe785be883
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-12 15:46:53 +0000

    Update CI scripts

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+set -e # halt script on error
 
 # The conversion of the notebooks is done via the nbjekyll package
 # https://github.com/trallard/nbconvert-jekyllconvert
+python -m nbjekyll.convert_nbs
+
+# build the site and test
+bundle exec jekyll build  # builds the site
+bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx # checks that there are no broke links
--- scripts/push_ghpages.sh
+++ scripts/push_ghpages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-SOURCE_BRANCH='backend'
 TARGET_BRANCH='master'
 
 # Save some useful information
@@ -9,9 +8,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+# Add the changes
+git add .
+git commit -m "Site checked and NB converted on Travis $TRAVIS_BUILD"
