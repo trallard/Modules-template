1da7fb913e129cbbe8d8f1eabbb4fe9bffa41c09
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-17 09:29:09 +0000

    Modified badge container

--- scripts/site_check.sh
+++ scripts/site_check.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build  # builds the site
-bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx # checks that there are no broke links 
+bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx # checks that there are no broken links 
