0e0dacb579761d330e04ffb6582d2dddbb615d8c
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-26 12:38:40 +0000

    Update travis

--- scripts/site_check.sh
+++ scripts/site_check.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build  # builds the site
-bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx # checks that there are no broken links 
+bundle exec htmlproofer ./_site --disable-external --check-favicon --only_4xx --ignore-file "/presentation/"# checks that there are no broken links 
