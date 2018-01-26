31e896d99b0cec28ac6fe2229620aee9b9622cc5
Tania Allard <t.allard@sheffield.ac.uk>
2018-01-26 12:47:36 +0000

    Update check for slides

--- scripts/site_check.sh
+++ scripts/site_check.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build  # builds the site
-bundle exec htmlproofer --disable-external --only_4xx --file-ignore "/reveal.js/, /presentation/" ./_site # checks that there are no broken links
+bundle exec htmlproofer --disable-external --only_4xx --file-ignore ["/reveal.js/", "/presentation/"] ./_site # checks that there are no broken links
